1299fb43e84dd6b2781b31886281835f11afa74c
RHEN <126537179+R-Hen7436@users.noreply.github.com>
2024-06-25 08:38:43 +0800

    Ross Henry Ubas
    To-do list Task

---
+++ script.js
@@ -0,0 +1,42 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addList(){
+    if(inputBox.value === ''){
+        alert("You did not input any list!!!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; //icon for x
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        const confirmDeletion = confirm("Are you sure to delete this list?");
+        if (confirmDeletion) {
+            e.target.parentElement.remove();
+            saveData();
+        }
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("Data", listContainer.innerHTML);
+}
+
+function showList(){
+    listContainer.innerHTML = localStorage.getItem("Data");
+}
+
+showList();
